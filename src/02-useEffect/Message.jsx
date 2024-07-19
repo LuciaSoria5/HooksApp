4beb5982e9c968bb54c7fc85100270117a6285c4
Lucia Soria <luciacsoria5@gmail.com>
2024-07-19 16:07:49 -0300

    Add <SimpleForm/> component

---
+++ src/02-useEffect/Message.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+export const Messagge = () => {
+
+    const [coords, setCoords] = useState({ x:0, y:0 });
+
+    useEffect(() => {
+      //console.log('Message Mounted') // cuando aparece el mensaje
+
+      const onMouseMove = ({ x, y }) => {
+        // const coords = { x, y };
+        setCoords({ x, y});    
+      }
+      // aca creamos el listener
+      window.addEventListener( 'mousemove', onMouseMove);
+    
+      return () => { //console.log('Message Unmounted') // cuando desaparece el mensaje
+        /* aca eliminamos el listener --> si no lo borramos, sigue ahi por mas 
+        que se borre el useEffect, y se crean mas cada vez que el mismo se ejecuta */ 
+        window.removeEventListener( 'mousemove', onMouseMove );
+      }
+
+    }, []);
+    
+  return (
+    <>
+        <h3>Usuario ya existe</h3>
+        <h5>{JSON.stringify(coords)}</h5>
+    </>
+  )
+};
---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+import { Messagge } from "./Message";
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'lucisoria@google.com'
+    });
+
+    const { username, email} = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState( {
+            ...formState,
+            [ name ]: value
+        });
+    };
+    // Es mejor tener efectos individuales por cada efecto secundario que querramos tener:
+    useEffect(() => { 
+        /* console.log("use effect called") */
+    }, []); // [] --> solo se ejecuta cuando el form es renderizado por primera vez
+    
+    useEffect(() => { 
+        /* console.log("formState changed") */
+    }, [ formState ]); // se ejecuta cada vez que cambia el form
+
+    useEffect(() => { 
+        /*console.log("email changed") */
+    }, [ email ]); // se ejecuta cada vez que cambia el email del form
+
+  return (
+    <>
+        <h1>Formulario simple</h1>
+        <hr />
+
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value={ username }
+            onChange={ onInputChange }
+        />
+        <input
+            type="email"
+            className="form-control mt-2"
+            placeholder="luci@google.com"
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+        />
+
+        { 
+            username === 'strider2' && <Messagge />
+        }
+    </>
+  )
+};
