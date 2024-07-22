77427e3f6e3237c343cd40ac14fbf2876bbfcd2a
Lucia Soria <luciacsoria5@gmail.com>
2024-07-22 16:18:30 -0300

    Add <FocusScreen/> component

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { FormWithCustomHooks } from './02-useEffect/FormWithCustomHooks';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // {/*<CounterWithCustomHook/>*/}
     // <SimpleForm />
     // <FormWithCustomHooks />
-    <MultipleCustomHooks />
+    // <MultipleCustomHooks />
+    <FocusScreen />
   // </React.StrictMode>,
 );
