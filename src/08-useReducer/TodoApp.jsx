50e253164a78acdc6427570744734bb5332cd94d
Lucia Soria <luciacsoria5@gmail.com>
2024-07-23 17:30:02 -0300

    Add useTodos custom hook

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,51 +1,13 @@
-import { useEffect, useReducer } from "react"
-import { todoReducer, TodoList, TodoAdd } from "./";
-
-const initialState = [];
-
-const init = () => {
-    // intenta parsear lo que haya en el valor de la calave 'todos' en el local storage, 
-    // y si da error retorna un arreglo vacío
-    return  JSON.parse( localStorage.getItem( 'todos' )) || [];
-}
+import { TodoList, TodoAdd } from "./";
+import { useTodo } from "../hooks";
 
 export const TodoApp = () => {
 
-                            // pasamos la referencia del todoReducer, no lo ejecutamos
-    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
-
-    // Almacenar en el localStorage nuestros todos, para que no se pierdan cuando recargamos el navegador
-    // actualizamos el localStorage cada vez que se actualice el todos
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify( todos ) );
-    }, [todos]); 
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleNewTodo, handleToggleTodo } = useTodo( );
     
-
-    const handleNewTodo = ( todo ) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        };
-        dispatch( action );
-    };
-
-    const handleDeleteTodo = ( id ) => {
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        });
-    };
-
-    const handleToggleTodo = ( id ) => {
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        });
-    };
-
     return (
         <>
-            <h1>TodoApp: 10, <small>pedientes: 2</small></h1>
+            <h1>Total de tareas: { todosCount }, <small>pedientes: { pendingTodosCount }</small></h1>
             <hr />
 
             <div className="row">
---
+++ src/hooks/useTodos.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer";
+
+
+// intenta parsear lo que haya en el valor de la calave 'todos' en el local storage,  y si da error retorna un arreglo vacío
+const init = () => JSON.parse( localStorage.getItem( 'todos' )) || [];
+const initialState = [];
+
+export const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init ); // pasamos la referencia del todoReducer, no lo ejecutamos
+
+    // Almacenar en el localStorage nuestros todos, para que no se pierdan cuando recargamos el navegador
+    // actualizamos el localStorage cada vez que se actualice el todos
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ) );
+    }, [todos]); 
+    
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        };
+        dispatch( action );
+    };
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    };
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    };
+
+    const pendingTodosCount = todos.filter( todo => !todo.done ).length;
+    const todosCount = todos.length;
+
+  return {
+    handleDeleteTodo,
+    handleNewTodo,
+    handleToggleTodo,
+    pendingTodosCount,
+    todos,
+    todosCount,
+  };
+};
